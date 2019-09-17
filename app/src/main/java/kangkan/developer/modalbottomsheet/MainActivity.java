fb860e4ecc4e5e6cbaa906ea4b497a7d91d9b866
kangkan <kangkankarmaker00@gmail.com>
2019-09-17 20:01:02 +0600

    Initial commit

---
+++ app/src/main/java/kangkan/developer/modalbottomsheet/MainActivity.java
@@ -0,0 +1,34 @@
+package kangkan.developer.modalbottomsheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements ExampleBottomSheetDialog.BottomSheetListener{
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.text_view_button_clicked);
+
+        Button buttonOpenBottomSheet = findViewById(R.id.button_open_bottom_sheet);
+        buttonOpenBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ExampleBottomSheetDialog bottomSheet = new ExampleBottomSheetDialog();
+                bottomSheet.show(getSupportFragmentManager(), "exampleBottomSheet");
+            }
+        });
+    }
+
+    @Override
+    public void onButtonClicked(String text) {
+        mTextView.setText(text);
+    }
+}
